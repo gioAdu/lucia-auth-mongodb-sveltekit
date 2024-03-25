887fd985f802dabae109652b4130cc255b2cb821
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-03-25 16:11:39 +0400

    Refactor Header component to add dropdown menu and update styling

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,30 +1,58 @@
 <script>
 	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 	import { enhance } from '$app/forms';
+	export let session, categories;
 
-	export let session;
-	console.log(session);
+	let showDropdown = false;
+	let timeoutId;
+
+	const hideDropDownAfterDelay = () => {
+		timeoutId = setTimeout(() => {
+			showDropdown = false;
+		}, 300);
+	};
 </script>
 
 <AppBar>
-	<div class="flex items-center gap-4" slot="lead">
-		<a href="/"><img src="/home_logo.png" alt="buyblitz" class="min-w-32 w-40" /></a>
-		<a href="/protected" class="btn bg-gradient-to-br variant-gradient-secondary-tertiary"
-			>PROTECTED</a
-		>
-		<a href="/not_protected" class="btn bg-gradient-to-br variant-gradient-secondary-tertiary"
-			>NOT PROTECTED</a
-		>
+	<div slot="lead">
+		<a href="/"> <img src="/home_logo.webp" alt="buyblitz" class="min-w-32 w-40" /> </a>
 	</div>
 
 	<div class="flex items-center gap-3" slot="trail">
+		<div
+			class="dropdown relative"
+			role="button"
+			tabindex="0"
+			on:mouseenter={() => {
+				clearTimeout(timeoutId);
+				showDropdown = true;
+			}}
+			on:mouseleave={hideDropDownAfterDelay}
+		>
+			<button class="btn bg-gradient-to-br variant-gradient-secondary-tertiary flex items-center">
+				<span>Shop</span>
+				<img src="/dropdown_arrow.svg" alt="dropdown arrow" class:rotate-90={showDropdown} class="transition-all" />
+			</button>
+			<ul
+				class:hidden={!showDropdown}
+				class="absolute bg-surface-50-900-token grid grid-cols-4 gap-x-10 right-0 min-w-max p-2 pt- rounded-lg border"
+			>
+				{#each categories as item}
+					<li class="border-b border-slate-700">
+						<a class="p-2 inline-block w-full" on:click={() => (showDropdown = false)} href={item}
+							>{item}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		</div>
+
 		{#if session}
-			<form method="post" action="/" use:enhance class="inline">
+			<form method="post" action="/" use:enhance class="inline ms-4">
 				<button class="btn bg-gradient-to-br variant-gradient-warning-error">Logout</button>
 			</form>
 		{:else}
-			<a href="/signin" class="btn bg-gradient-to-br variant-gradient-secondary-tertiary">Log In</a>
-			<a href="/signup" class="btn variant-filled-secondary font-bold">Sign Up</a>
+			<a href="/signup" class="btn variant-filled-primary font-bold ms-4">Log In</a>
 		{/if}
 		<LightSwitch />
 	</div>
