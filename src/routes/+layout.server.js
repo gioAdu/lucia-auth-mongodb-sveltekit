379137f308cbc5757a6c5b2c52b321627d1cf06c
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-03-25 16:11:44 +0400

    Add categoriesData to layout server route

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,8 @@
 export const load = async (event) => {
 	const session = event.locals.session;
 
-	return { session };
+	const categories = await fetch('https://dummyjson.com/products/categories');
+	const categoriesData = await categories.json();
+
+	return { session, categoriesData };
 };
