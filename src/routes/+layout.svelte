1b8c0f97beeae1d61355244c51edbaa66edb1764
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-03-25 18:28:10 +0400

    Add LightSwitch component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script>
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 	import Header from '../lib/components/Header.svelte';
-
+	
 	export let data;
 </script>
 
 <Header session={data.session} categories = {data.categoriesData}/>
 
 <slot />
+
+<div class="fixed bottom-2 right-2">
+	<LightSwitch />
+</div>
+
