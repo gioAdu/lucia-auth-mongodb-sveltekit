99e6e1bde1b54dc24011e2d01989a6f193271cb0
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-03-27 18:46:11 +0400

    Add cart functionality and button animation to category page

--- src/routes/[categories]/+page.svelte
+++ src/routes/[categories]/+page.svelte
@@ -1,17 +1,43 @@
 <script>
+	import { onMount } from 'svelte';
+	import { count } from '../../lib/stores/store.js';
+
 	export let data;
+	let cartItems = [];
 
-	console.log(data.catData);
+	onMount(() => {
+		cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+	});
 
-	const addToCart = (e) => {
+	const addToCart = (e, id, category) => {
 		e.preventDefault();
+		count.update((n) => n + 1);
+
+		if (!data.session) {
+			const existingItem = cartItems.find((item) => item.id === id && item.category === category);
+
+			if (existingItem) {
+				existingItem.count += 1;
+			} else {
+				cartItems.push({ id, category, count: 1 });
+			}
+			localStorage.setItem('cart', JSON.stringify(cartItems));
+		}
+	};
+
+	const btnAnimation = (e,size) => {
+		const button = e.currentTarget;
+		button.style.transform = `scale(${size})`;
+		setTimeout(() => {
+			button.style.transform = 'scale(1)';
+		}, 300);
 	};
 </script>
 
 <div class="flex flex-wrap container m-auto">
 	{#each data?.catData as item}
-		<a href="/categories/{item.id}" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 flex">
-			<div class="card card-hover variant-ringed flex flex-col w-full">
+		<div  class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 flex">
+			<a href="/categories/{item.id}" class="card card-hover variant-ringed flex flex-col w-full">
 				<header class="card-header"><img src={item.thumbnail} alt={item.title} /></header>
 				<section class="p-4 flex-grow">
 					<div class="card-body">
@@ -28,12 +54,18 @@
 					</div>
 				</section>
 				<footer class="card-footer flex flex-col items-end justify-center">
-					<button class="flex flex-col items-center scale-90" on:click={addToCart}>
+					<button
+						class="flex flex-col items-center scale-90 transition-transform duration-300"
+						on:click={(event) => {
+							addToCart(event, item.id, item.category);
+							btnAnimation(event,1.15);
+						}}
+					>
 						<img src="/cart.svg" alt="cart" />
 						<div class="font-bold text-tertiary-900-50-token">Add To Cart</div>
 					</button>
 				</footer>
-			</div>
-		</a>
+			</a>
+		</div>
 	{/each}
 </div>
