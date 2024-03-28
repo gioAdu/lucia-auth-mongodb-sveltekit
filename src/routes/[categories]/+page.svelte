2eca15e5e1077cf6d000cc01b28c0fdeee72e414
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-03-28 15:15:12 +0400

    Fix formatting and add spacing in +page.svelte

--- src/routes/[categories]/+page.svelte
+++ src/routes/[categories]/+page.svelte
@@ -25,7 +25,7 @@
 		}
 	};
 
-	const btnAnimation = (e,size) => {
+	const btnAnimation = (e, size) => {
 		const button = e.currentTarget;
 		button.style.transform = `scale(${size})`;
 		setTimeout(() => {
@@ -36,29 +36,35 @@
 
 <div class="flex flex-wrap container m-auto">
 	{#each data?.catData as item}
-		<div  class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 flex">
+		<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 flex">
 			<a href="/categories/{item.id}" class="card card-hover variant-ringed flex flex-col w-full">
-				<header class="card-header"><img src={item.thumbnail} alt={item.title} /></header>
+				<header class="card-header">
+					<img src={item.thumbnail} alt={item.title} />
+				</header>
+
 				<section class="p-4 flex-grow">
 					<div class="card-body">
 						<h1 class="card-title text-xl flex justify-between mb-2 border-t">
 							{item.title}
 							<span class="card-price">$10</span>
 						</h1>
+						
 						<div class="card-brand">
 							<span class="font-bold text-lg">Brand :</span>
 							{item.brand}
 						</div>
+						
 						<div class="mt-2 font-bold text-lg">Description :</div>
 						<div class="card-description">{item.description}</div>
 					</div>
 				</section>
+
 				<footer class="card-footer flex flex-col items-end justify-center">
 					<button
 						class="flex flex-col items-center scale-90 transition-transform duration-300"
 						on:click={(event) => {
 							addToCart(event, item.id, item.category);
-							btnAnimation(event,1.15);
+							btnAnimation(event, 1.15);
 						}}
 					>
 						<img src="/cart.svg" alt="cart" />
