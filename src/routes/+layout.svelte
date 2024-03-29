7ae906f2862d9a9179e7a66873f58fb4151899ae
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-03-29 15:05:15 +0400

    Update layout.svelte: Adjusted right margin of fixed div

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,14 @@
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 	import Header from '../lib/components/Header.svelte';
-	
+
 	export let data;
 </script>
 
-<Header session={data.session} categories = {data.categoriesData}/>
+<Header session={data.session} categories={data.categoriesData} />
 
 <slot />
 
-<div class="fixed bottom-2 right-2">
+<div class="fixed bottom-2 right-4">
 	<LightSwitch />
 </div>
-
