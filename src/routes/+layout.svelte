506de58327f63d62054f64add31ce6cce2d68a3d
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-03-29 16:21:36 +0400

    Add cartItems to localStorage on page load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,14 @@
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 	import Header from '../lib/components/Header.svelte';
+	import { onMount } from 'svelte';
+	import { cartItems } from '../lib/stores/store';
 
 	export let data;
+
+	onMount(() => {
+		$cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+	});
 </script>
 
 <Header session={data.session} categories={data.categoriesData} />
