6c3c4c98d27d804aa58689d94c17041a1c3fe3b8
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-03 18:53:52 +0400

    Add cart functionality to +page server route

--- src/routes/[categories]/+page.server.js
+++ src/routes/[categories]/+page.server.js
@@ -1,3 +1,6 @@
+import { fail } from '@sveltejs/kit';
+import { dataMap } from '$lib/helpers/dataMap.js';
+
 export async function load(event) {
 	const categorieId = event.params.categories;
 	const catDataJson = await fetch(`https://dummyjson.com/products/category/${categorieId}`);
@@ -5,3 +8,21 @@ export async function load(event) {
 
 	return { cat: event.params.categories, catData: catData.products };
 }
+
+export const actions = {
+	default: async ({ locals, request }) => {
+		const data = await request.formData();
+ 
+		const cartItems = data.get('cartItems')
+
+		if (!locals.session) {
+			return fail(401, { session: false, message: 'Unauthorized' });
+		}
+
+		try {
+			await dataMap(cartItems, locals.session.userId);
+		} catch (error) {
+			return fail(500, { message: error.message });
+		}
+	}
+};
