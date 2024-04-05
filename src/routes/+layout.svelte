2f56c5fd0b15be9480d45c4ad4ab0e01dd89f41a
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-05 16:17:04 +0400

    Refactor +layout.svelte to import and initialize serverCartItems store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,24 @@
 <script>
-	import { LightSwitch } from '@skeletonlabs/skeleton';
+	import { LightSwitch, Toast } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 	import Header from '../lib/components/Header.svelte';
 	import { onMount } from 'svelte';
-	import { cartItems } from '../lib/stores/store';
+	import { cartItems, serverCartItems } from '../lib/stores/store';
+	import { initializeStores } from '@skeletonlabs/skeleton';
 
 	export let data;
 
 	onMount(() => {
-		$cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+		cartItems.set(JSON.parse(localStorage.getItem('cart')) || []);
+		serverCartItems.set(data.cartServer)
 	});
+	initializeStores();
 </script>
 
+<Toast position="tl" spacing="gap-4" />
+
 <Header session={data.session} categories={data.categoriesData} />
-
 <slot />
-
 <div class="fixed bottom-2 right-2">
 	<LightSwitch />
 </div>
