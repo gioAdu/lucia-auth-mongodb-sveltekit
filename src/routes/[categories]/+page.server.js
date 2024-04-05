389ccc772aa097b2c642b34b9ba871698d7c90d3
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-05 16:17:08 +0400

    Refactor +page.server.js to add cart functionality and improve error handling

--- src/routes/[categories]/+page.server.js
+++ src/routes/[categories]/+page.server.js
@@ -1,5 +1,6 @@
 import { fail } from '@sveltejs/kit';
 import { dataMap } from '$lib/helpers/dataMap.js';
+import { getCartFromServer } from '$lib/helpers/serverCart.js';
 
 export async function load(event) {
 	const categorieId = event.params.categories;
@@ -12,17 +13,34 @@ export async function load(event) {
 export const actions = {
 	default: async ({ locals, request }) => {
 		const data = await request.formData();
- 
-		const cartItems = data.get('cartItems')
+		const userId = locals.session.userId;
+
+		const cartItems = data.get('cart_data');
 
 		if (!locals.session) {
 			return fail(401, { session: false, message: 'Unauthorized' });
 		}
 
+		//Add item to cart on server
 		try {
-			await dataMap(cartItems, locals.session.userId);
+			await dataMap(cartItems, userId);
 		} catch (error) {
-			return fail(500, { message: error.message });
+			return fail(500, {
+				serverErr: true,
+				message: 'Unable to add item. Please try again later'
+			});
+		}
+
+		//Get cart items from server
+		try {
+			const serverCart = await getCartFromServer(userId);
+			return { success: true, serverCart };
+		} catch (error) {
+			console.log(error);
+			return fail(500, {
+				serverErr: true,
+				message: 'Something went wrong'
+			});
 		}
 	}
 };
