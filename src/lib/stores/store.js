ac8043dd9edc7035c1af763afa48f6c6e62db20a
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-05 16:16:57 +0400

    Add serverCartItems store to store.js

--- src/lib/stores/store.js
+++ src/lib/stores/store.js
@@ -1,7 +1,12 @@
 import { derived, writable } from 'svelte/store';
 
 export const cartItems = writable([]);
+export const serverCartItems = writable([]);
 
-export const derivedCount = derived(cartItems, $cartItems => 
+export const derivedCount = derived(cartItems, ($cartItems) =>
 	$cartItems.reduce((total, item) => total + item?.count, 0)
+);
+
+export const derivedServerCount = derived(serverCartItems, ($serverCartItems) =>
+	$serverCartItems.reduce((total, item) => total + item?.count, 0)
 );
