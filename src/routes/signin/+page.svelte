b7350158582698061a3d4e809af83a05df5cdb91
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-05 16:17:19 +0400

    Refactor +page.svelte to improve error handling and add cart functionality

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,36 +1,85 @@
 <script>
 	import { enhance } from '$app/forms';
-	export let form;
-  
-	$: usernameError = form?.message.includes('Username') ? 'input-error' : '';
-	$: passwordError = form?.message.includes('Password') ? 'input-error' : '';
+	import { onMount } from 'svelte';
+	import { cartItems } from '$lib/stores/store.js';
+	import { goto, invalidateAll } from '$app/navigation';
+
+	let usernameError;
+	let passwordError;
+	let cart;
+	let loading = false;
+
+	const displayErrMsg = (errMsg) => {
+		if (errMsg.includes('Username')) {
+			usernameError = errMsg;
+		} else {
+			usernameError = '';
+		}
+
+		if (errMsg.includes('Password')) {
+			passwordError = errMsg;
+		} else {
+			passwordError = '';
+		}
+	};
+
+	const clearLocalCart = () => {
+		localStorage.removeItem('cart');
+		cartItems.set([]);
+	};
+
+	onMount(() => {
+		cart = localStorage.getItem('cart');
+	});
 </script>
 
 <h1 class="p-2">Sign in</h1>
 
-<form method="post" use:enhance class="p-2 w-fit">
+<form
+	method="post"
+	class="p-2 w-fit"
+	use:enhance={() => {
+		loading = true;
+		return async ({ result }) => {
+			if (result.type === 'failure') {
+				loading = false;
+				displayErrMsg(result.data.message);
+			}
+
+			if (result.type === 'success') {
+				clearLocalCart();
+				await invalidateAll();
+				loading = false;
+				goto('/');
+			}
+		};
+	}}
+>
+	<input type="hidden" value={cart} name="cartItems" />
 	<div class=" mb-2">
 		<input
-			class="block variant-form-material {usernameError}"
+			class="block variant-form-material"
+			class:input-error={usernameError}
 			placeholder="username"
 			name="username"
 		/>
 		{#if usernameError}
-			<p class="text-error-500">{form?.message}</p>
+			<p class="text-error-500">{usernameError}</p>
 		{/if}
 	</div>
 
 	<div class="mb-2">
 		<input
-			class="block variant-form-material {passwordError}"
+			class="block variant-form-material"
+			class:input-error={passwordError}
 			placeholder="password"
 			type="password"
 			name="password"
 		/>
 		{#if passwordError}
-			<p class="text-error-500">{form?.message}</p>
+			<p class="text-error-500">{passwordError}</p>
 		{/if}
 	</div>
 
-	<button class="btn variant-filled w-full">Continue</button>
+	<button class="btn variant-filled w-full" disabled = {loading}>Log in</button>
 </form>
