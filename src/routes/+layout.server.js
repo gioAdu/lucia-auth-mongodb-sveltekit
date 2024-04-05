9c6054a018ae665d5a20380cfa6dce44a191e084
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-05 16:17:01 +0400

    Refactor +layout.server.js to use getCartFromServer and improve error handling

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,20 @@
-export const load = async (event) => {
-	const session = event.locals.session;
+import { getCartFromServer } from '$lib/helpers/serverCart.js';
+import { fail } from '@sveltejs/kit';
+
+export const load = async ({ fetch, locals }) => {
+	const session = locals.session;
+	let cartServer;
 
 	const categories = await fetch('https://dummyjson.com/products/categories');
 	const categoriesData = await categories.json();
 
-	return { session, categoriesData };
+	if (session) {
+		try {
+			cartServer = await getCartFromServer(session.userId);
+		} catch (error) {
+			return fail(500, { message: 'Could not get cart data' });
+		}
+	}
+
+	return { session, categoriesData, cartServer };
 };
