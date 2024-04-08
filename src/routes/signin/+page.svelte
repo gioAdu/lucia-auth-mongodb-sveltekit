70af922a0e084bb49a0183e0d3c70bd4d3cdfa12
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-08 13:48:43 +0400

    Update form validation and error handling in +page.svelte

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -3,25 +3,11 @@
 	import { onMount } from 'svelte';
 	import { cartItems } from '$lib/stores/store.js';
 	import { goto, invalidateAll } from '$app/navigation';
+	import { displayErrMsg } from '$lib/helpers/displayErrMsg';
 
-	let usernameError;
-	let passwordError;
 	let cart;
 	let loading = false;
-
-	const displayErrMsg = (errMsg) => {
-		if (errMsg.includes('Username')) {
-			usernameError = errMsg;
-		} else {
-			usernameError = '';
-		}
-
-		if (errMsg.includes('Password')) {
-			passwordError = errMsg;
-		} else {
-			passwordError = '';
-		}
-	};
+	let errMsg;
 
 	const clearLocalCart = () => {
 		localStorage.removeItem('cart');
@@ -33,53 +19,59 @@
 	});
 </script>
 
-<h1 class="p-2">Sign in</h1>
+<div class="flex flex-col items-center mt-12">
+	<h1 class="p-2 text-4xl mb-2">Sign in</h1>
 
-<form
-	method="post"
-	class="p-2 w-fit"
-	use:enhance={() => {
-		loading = true;
-		return async ({ result }) => {
-			if (result.type === 'failure') {
-				loading = false;
-				displayErrMsg(result.data.message);
-			}
+	<form
+		method="post"
+		class="p-2 w-full max-w-lg"
+		use:enhance={() => {
+			loading = true;
+			return async ({ result }) => {
+				if (result.type === 'failure') {
+					loading = false;
+					errMsg = displayErrMsg(result.data.message);
+					console.log(errMsg);
+				}
 
-			if (result.type === 'success') {
-				clearLocalCart();
-				await invalidateAll();				
-				loading = false;
-				goto('/');
-			}
-		};
-	}}
->
-	<input type="hidden" value={cart} name="cartItems" />
-	<div class=" mb-2">
-		<input
-			class="block variant-form-material"
-			class:input-error={usernameError}
-			placeholder="username"
-			name="username"
-		/>
-		{#if usernameError}
-			<p class="text-error-500">{usernameError}</p>
-		{/if}
-	</div>
+				if (result.type === 'success') {
+					clearLocalCart();
+					await invalidateAll();
+					loading = false;
+					goto('/');
+				}
+			};
+		}}
+	>
+		<input type="hidden" value={cart} name="cartItems" />
+		<div class=" mb-2">
+			<input
+				class="input w-full"
+				class:input-error={errMsg?.usernameError}
+				placeholder="Username"
+				name="username"
+			/>
+			{#if errMsg?.usernameError}
+				<p class="text-error-500">{errMsg?.usernameError}</p>
+			{/if}
+		</div>
 
-	<div class="mb-2">
-		<input
-			class="block variant-form-material"
-			class:input-error={passwordError}
-			placeholder="password"
-			type="password"
-			name="password"
-		/>
-		{#if passwordError}
-			<p class="text-error-500">{passwordError}</p>
-		{/if}
-	</div>
+		<div class="mb-2">
+			<input
+				class="input w-full"
+				class:input-error={errMsg?.passwordError}
+				placeholder="Password"
+				type="password"
+				name="password"
+			/>
+			{#if errMsg?.passwordError}
+				<p class="text-error-500">{errMsg?.passwordError}</p>
+			{/if}
+		</div>
 
-	<button class="btn variant-filled w-full" disabled = {loading}>Log in</button>
-</form>
+		<div class="flex gap-2">
+			<button class="btn variant-filled-primary w-full" disabled={loading}>Log in</button>
+			<a href="/signup" class="btn variant-filled-secondary w-full">Sign up</a>
+		</div>
+	</form>
+</div>
