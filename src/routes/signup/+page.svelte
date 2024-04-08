7eb904d1c0121023cedf1ab14aca21469fccd8e4
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-08 13:48:51 +0400

    Refactor form validation and error handling in +page.svelte

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,36 +1,65 @@
 <script>
 	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
+	import { displayErrMsg } from '$lib/helpers/displayErrMsg';
 
-	export let form;
-
-	$: usernameError = form?.message.includes('Username') ? 'input-error' : '';
-	$: passwordError = form?.message.includes('Password') ? 'input-error' : '';
+	let loading;
+	let errMsg;
 </script>
 
-<h1 class="p-2">Sign up</h1>
-<form method="post" use:enhance class="p-2 w-fit">
-	<div class=" mb-2">
-		<input
-			class="block variant-form-material {usernameError}"
-			placeholder="username"
-			name="username"
-		/>
-		{#if usernameError}
-			<p class="text-error-500">{form?.message}</p>
-		{/if}
-	</div>
+<div class="flex flex-col items-center mt-12">
+	<h1 class="p-2 text-4xl mb-2">Sign up</h1>
+	<form
+		method="post"
+		use:enhance={() => {
+			loading = true;
+			return async ({ result }) => {
+				if (result.type === 'failure') {
+					loading = false;
+					errMsg = displayErrMsg(result.data.message);
+				}
 
-	<div class="mb-2">
-		<input
-			class="block variant-form-material {passwordError}"
-			placeholder="password"
-			type="password"
-			name="password"
-		/>
-		{#if passwordError}
-			<p class="text-error-500">{form?.message}</p>
-		{/if}
-	</div>
+				if (result.type === 'success') {
+					loading = false;
+					goto('/signin');
+				}
+			};
+		}}
+		class="p-2 w-full max-w-lg"
+	>
+		<div class=" mb-2">
+			<input
+				class:input-error={errMsg?.usernameError}
+				class="input w-full"
+				placeholder="Username"
+				name="username"
+			/>
+			{#if errMsg?.usernameError}
+				<p class="text-error-500">{errMsg?.usernameError}</p>
+			{/if}
+		</div>
 
-	<button class="btn variant-filled w-full">Continue</button>
-</form>
+		<div class="mb-2">
+			<input
+				class:input-error={errMsg?.passwordError}
+				class="input w-full"
+				placeholder="Password"
+				type="password"
+				name="password"
+			/>
+			{#if errMsg?.passwordError}
+				<p class="text-error-500">{errMsg?.passwordError}</p>
+			{/if}
+		</div>
+
+		<div class="flex gap-2">
+			<button class="btn variant-filled-secondary w-full" disabled={loading}>Sign Up</button>
+			<a
+				href="/signin"
+				class="btn variant-filled-primary w-full"
+				class:pointer-events-none={loading}
+				class:opacity-50={loading}>Log In</a
+			>
+		</div>
+	</form>
+</div>
