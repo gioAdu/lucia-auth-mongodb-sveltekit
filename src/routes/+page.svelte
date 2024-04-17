20482b405273671fd1700a210703ac49d2254153
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-17 17:48:50 +0400

    Update import path for cartItems in +layout.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
+<script>
+	import Card from '$lib/components/Card.svelte';
+
+	export let data;
+	const products = data.products;
+</script>
+
 <div class="bg-[url('/banner.avif')] bg-no-repeat bg-cover">
 	<div class="container mx-auto px-2 h-[400px] text-white">
 		<h1 class="text-5xl mb-8 pt-16">Explore Everything You Need</h1>
 		<div class="text-2xl">Your one-stop-shop for style, tech, and beyond</div>
-    <a href="/categories" class="text-center block mt-8 variant-filled-success px-5 py-3 w-1/4 text-xl rounded-full">Check our shop</a>
+		<a
+			href="/categories"
+			class="text-center block mt-8 variant-filled-success px-5 py-3 w-1/4 text-xl rounded-full"
+			>Check our shop</a
+		>
 	</div>
 </div>
+
+<Card productdata={products} session={data?.session} />
