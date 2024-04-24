6a2aa2d56ac930c038bd81d505a6df4d869b564b
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-24 16:50:14 +0400

    Add InfiniteScroll component to InfiniteScroll.svelte

---
+++ src/lib/misc/InfiniteScroll.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { onMount, onDestroy, createEventDispatcher } from 'svelte';
+
+	export let threshold = 0;
+	export let elementScroll = null;
+	export let hasMore = true;
+
+	const dispatch = createEventDispatcher();
+	let observer;
+
+	console.log(hasMore);
+
+	onMount(() => {
+		if (elementScroll) {
+			observer = new IntersectionObserver(onIntersection, {
+				root: null, // use the viewport as the root
+				rootMargin: '0px',
+				threshold: threshold
+			});
+
+			observer.observe(elementScroll);
+		}
+	});
+
+	onDestroy(() => {
+		if (observer) {
+			observer.disconnect();
+		}
+	});
+
+	const onIntersection = (entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				if (hasMore) {
+					dispatch('loadMore');
+				}
+			}
+		});
+	};
+</script>
+
+<div bind:this={elementScroll} />
