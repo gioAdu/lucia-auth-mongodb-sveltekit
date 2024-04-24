620d10ed23dabe792a00c3ef8337d8d5c9e43dc1
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-24 16:59:09 +0400

    Refactor BurgerMenu component to wrap it in a div container

--- src/lib/components/BurgerMenu.svelte
+++ src/lib/components/BurgerMenu.svelte
@@ -2,19 +2,21 @@
 	import { TreeView, TreeViewItem } from '@skeletonlabs/skeleton';
 </script>
 
-<TreeView>
-	<TreeViewItem>
-		(item 1)
-		<svelte:fragment slot="children">
-			<TreeViewItem>
-				(Child 1)
-				<svelte:fragment slot="children">
-					<TreeViewItem>(Child of Child 1)</TreeViewItem>
-					<TreeViewItem>(Child of Child 2)</TreeViewItem>
-				</svelte:fragment>
-			</TreeViewItem>
-			<TreeViewItem>(Child 2)</TreeViewItem>
-		</svelte:fragment>
-	</TreeViewItem>
-	<TreeViewItem>(item 2)</TreeViewItem>
-</TreeView>
+<div class="pt-2">
+	<TreeView>
+		<TreeViewItem>
+			(item 1)
+			<svelte:fragment slot="children">
+				<TreeViewItem>
+					(Child 1)
+					<svelte:fragment slot="children">
+						<TreeViewItem>(Child of Child 1)</TreeViewItem>
+						<TreeViewItem>(Child of Child 2)</TreeViewItem>
+					</svelte:fragment>
+				</TreeViewItem>
+				<TreeViewItem>(Child 2)</TreeViewItem>
+			</svelte:fragment>
+		</TreeViewItem>
+		<TreeViewItem>(item 2)</TreeViewItem>
+	</TreeView>
+</div>
