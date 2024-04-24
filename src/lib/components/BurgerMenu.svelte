59ab58d076a0f7c42fb826f7f7d407a76f7d7b54
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-24 16:49:36 +0400

    Add BurgerMenu component to project

---
+++ src/lib/components/BurgerMenu.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { TreeView, TreeViewItem } from '@skeletonlabs/skeleton';
+</script>
+
+<TreeView>
+	<TreeViewItem>
+		(item 1)
+		<svelte:fragment slot="children">
+			<TreeViewItem>
+				(Child 1)
+				<svelte:fragment slot="children">
+					<TreeViewItem>(Child of Child 1)</TreeViewItem>
+					<TreeViewItem>(Child of Child 2)</TreeViewItem>
+				</svelte:fragment>
+			</TreeViewItem>
+			<TreeViewItem>(Child 2)</TreeViewItem>
+		</svelte:fragment>
+	</TreeViewItem>
+	<TreeViewItem>(item 2)</TreeViewItem>
+</TreeView>
