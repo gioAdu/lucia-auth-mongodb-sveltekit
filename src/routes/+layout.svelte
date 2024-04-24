d4fc612804ffdb5963f7ba8f296f8ee236f7f79b
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-24 16:50:46 +0400

    Update +layout.svelte to add BurgerMenu component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
-	import { LightSwitch, Toast } from '@skeletonlabs/skeleton';
+	import { Drawer, Toast } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 	import Header from '$lib/components/Header.svelte';
 	import { onMount } from 'svelte';
 	import { cartItems, serverCartItems } from '$lib/stores/store';
 	import { initializeStores } from '@skeletonlabs/skeleton';
+	import BurgerMenu from '$lib/components/BurgerMenu.svelte';
 
 	export let data;
 
@@ -17,12 +18,15 @@
 	}
 
 	initializeStores();
+	
 </script>
+<Drawer> 
+	<BurgerMenu/>
+</Drawer>
 
 <Toast position="tl" spacing="gap-4" />
+
 
 <Header session={data.isLoggedIn} categories={data.categoriesData} />
 
 <slot />
-
-
