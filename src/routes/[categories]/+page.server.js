305d3bda90d7309b80368c4b23f950d333e1b083
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-04-24 16:54:23 +0400

    Update +page.server.js to fetch initial products

--- src/routes/[categories]/+page.server.js
+++ src/routes/[categories]/+page.server.js
@@ -1,6 +1,14 @@
 export async function load(event) {
 	const categorieId = event.params.categories;
-	const catDataJson = await fetch(`https://dummyjson.com/products/category/${categorieId}`);
+	let catDataJson = [];
+
+	if (categorieId === 'categories') {
+		catDataJson = await fetch(`https://dummyjson.com/products?limit=12&skip=12`);
+		const catData = await catDataJson.json();
+		return { cat: categorieId, catData: catData.products}
+	}
+
+	catDataJson = await fetch(`https://dummyjson.com/products/category/${categorieId}`);
 	const catData = await catDataJson.json();
 
 	return { cat: event.params.categories, catData: catData.products };
