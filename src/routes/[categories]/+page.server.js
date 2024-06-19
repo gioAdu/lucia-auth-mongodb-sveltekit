0e6b6b5900994a67caec5ab3492d71ed2180f15f
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-06-19 14:41:22 +0400

    chore: Update +page.server.js to fetch initial products with a specified limit

--- src/routes/[categories]/+page.server.js
+++ src/routes/[categories]/+page.server.js
@@ -1,15 +1,18 @@
 export async function load(event) {
 	const categorieId = event.params.categories;
 	let catDataJson = [];
+	let limit = 12;
 
 	if (categorieId === 'categories') {
-		catDataJson = await fetch(`https://dummyjson.com/products?limit=12&skip=12`);
+		catDataJson = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${limit}`);
 		const catData = await catDataJson.json();
-		return { cat: categorieId, catData: catData.products}
+		return { cat: categorieId, catData: catData.products };
 	}
 
-	catDataJson = await fetch(`https://dummyjson.com/products/category/${categorieId}`);
+	catDataJson = await fetch(
+		`https://dummyjson.com/products/category/${categorieId}?limit=${limit}`
+	);
 	const catData = await catDataJson.json();
 
-	return { cat: event.params.categories, catData: catData.products };
+	return { cat: event.params.categories, catData: catData.products, initialLimit: limit };
 }
