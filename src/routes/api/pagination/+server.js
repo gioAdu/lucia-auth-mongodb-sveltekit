963e0726e18fcd0b5082ef8cf5831d83adbde3fa
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-06-19 14:41:32 +0400

    chore: Update pagination server logic to fetch products by category if specified

--- src/routes/api/pagination/+server.js
+++ src/routes/api/pagination/+server.js
@@ -1,14 +1,22 @@
 import { json } from '@sveltejs/kit';
 
-export async function POST({request} ) {
+export async function POST({ request }) {
+	let newData = [];
 	const pageData = await request.json();
 
-	const {productCount, skip} = pageData;
+	const { productCount, skip, category } = pageData;
 
+	if (category) {
+		const newResp = await fetch(
+			`https://dummyjson.com/products/category/${category}?limit=${productCount}&skip=${skip}`
+		);
+		newData = await newResp.json();
+	} else {
+		const newResp = await fetch(
+			`https://dummyjson.com/products?limit=${productCount}&skip=${skip}`
+		);
+		newData = await newResp.json();
+	}
 
-  const newResp = await fetch(`https://dummyjson.com/products?limit=${productCount}&skip=${skip}`);
-  const newData = await newResp.json();
-
-
-	return json({ status: 200, newData : newData.products });
+	return json({ status: 200, newData: newData.products });
 }
